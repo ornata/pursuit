e1c30c3bcd89d27f59a4ad471a56d72f0e5fa583
Jessica Paquette <jlpaquet@uvic.ca>
2015-03-10 16:24:24 -0700

    that regular expression was a terrible idea. much faster now.

--- read_graph.py
+++ read_graph.py
@@ -1,17 +1,16 @@
-import re
 import sys
 import networkx as nx
 
 ## Reads in a digraph from stdin and returns it
-def read_graph(n, number):
+def read_graph(n):
     G = nx.DiGraph()
     G.add_nodes_from([x for x in range(0, n)]) # init G with n nodes
 
     nodes_read = 0 
 
     while nodes_read < n:
-        source = read_digit(number)
-        target = read_line(number)
+        source = read_digit()
+        target = read_line()
         edges = [(source,y) for y in target]
         G.add_edges_from(edges)
         nodes_read +=1
@@ -19,81 +18,79 @@ def read_graph(n, number):
     return G
 
 ## Read until reaching \n from the current position in the stdin buffer
-def read_line(number):
+def read_line():
     line = []
     ch = " "
     while ch != "\n" and ch != "":
         ch = sys.stdin.read(1)
-        if(re.match(number,ch)):
+        if(ch.isdigit()):
             line.append(int(ch))
     return line
 
 ## Read a single digit, ignoring all other characters
-def read_digit(number):
+## In the case of reaching EOF before completion, return -1
+def read_digit():
     n = sys.stdin.read(1)
     while n != "":
-        if(re.match(number,n)):
+        if(n.isdigit()):
             return int(n)
         n = sys.stdin.read(1)
     return -1
 
 ## Reads in a list of moves as tuples
-def read_move_list(n, number):
+def read_move_list(n):
     nodes_read = 0
     move_list = []
 
     while nodes_read < n:
 
-        source = read_digit(number)
+        # Get the vertex you start at
+        source = read_digit()
 
+        # Hit EOF, return because we're done
         if source == "":
             move_list
-            print "Ended at source"
-            print move_list
             return move_list
 
-        target = read_line(number)
+        # Get the list of vertices the source can go to
+        target = read_line()
 
+        # If there's nothing, then just return an empty list
         if target == []:
-            print "Ended at target"
-            print move_list
             return move_list
 
+        # Make the list of legal moves and append it to the move list
         source_moves = [(source,y) for y in target]
-        for move in source_moves:
-            move_list.append(move)
+        move_list.extend(source_moves)
         nodes_read += 1
-    print "Ended normally"
-    print move_list
+
     return move_list
 
 ## Reads in everything necessary to play the game and returns
 ## it all in a list
 def read_game():
-    # used in other read functions
-    number = re.compile("[-+]?\d+")
-    
+
     # number of players
-    left_players = read_digit(number)
-    right_players = read_digit(number)
+    left_players = read_digit()
+    right_players = read_digit()
 
     # read game graphs
-    left_nodes = read_digit(number)
-    left = read_graph(left_nodes, number)
+    left_nodes = read_digit()
+    left = read_graph(left_nodes)
 
-    right_nodes = read_digit(number)
-    right = read_graph(right_nodes, number)
+    right_nodes = read_digit()
+    right = read_graph(right_nodes)
 
     # read allowed moves
-    allowed_left = read_move_list(left_nodes, number)
-    allowed_right = read_move_list(left_nodes, number)
+    allowed_left = read_move_list(left_nodes)
+    allowed_right = read_move_list(left_nodes)
 
     # read final states
-    n_final_states = read_digit(number)
-    final_states = read_move_list(n_final_states, number)
+    n_final_states = read_digit()
+    final_states = read_move_list(n_final_states)
 
     # read starting positions
-    start_left = read_digit(number)
-    start_right = read_digit(number)
+    start_left = read_digit()
+    start_right = read_digit()
 
     return [left,right,allowed_left,allowed_right,final_states,start_left,start_right]
